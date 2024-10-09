0fb29ed8e2511356fc62d7d2314dd53c02cb1a61
André Klarpås <andrefkl@ntnu.no>
2024-10-09 15:37:49 +0200

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+if [ -z "$1" ]; then
+  echo "No environment specified, defaulting to staging"
+  ENVIRONMENT="staging"
+else
+  ENVIRONMENT=$1
+fi
+
+# Default profile
+AWS_PROFILE="default"
+
+# Parse arguments:  ---- Example, now its possible to run: npm run deploy -- --profile=dotkom
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --profile=*) AWS_PROFILE="${1#*=}" ;;
+        *) ENV="$1" ;; # Assuming the first non-option argument is the environment
+    esac
+    shift
+done
+
+
+doppler setup -c $ENVIRONMENT -p voting-frontend
+if [ $? -ne 0 ]; then
+  echo "Failed to setup Doppler"
+  exit 1
+fi
+
+FRONTEND_S3_BUCKET=$(doppler run --command 'echo $FRONTEND_S3_BUCKET')
+CLOUDFRONT_DISTRIBUTION_ID=$(doppler run --command 'echo $CLOUDFRONT_DISTRIBUTION_ID')
+
+doppler run yarn build &&
+aws s3 rm $FRONTEND_S3_BUCKET --recursive --profile $AWS_PROFILE &&
+aws s3 cp build $FRONTEND_S3_BUCKET --recursive --profile $AWS_PROFILE &&
+aws cloudfront create-invalidation --distribution-id $CLOUDFRONT_DISTRIBUTION_ID --profile $AWS_PROFILE --paths  '/*'
