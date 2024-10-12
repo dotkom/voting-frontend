2212783caca621c866b4dd8447d972f731d04781
Jo Gramnæs Tjernshaugen <jotjernshaugen@gmail.com>
2024-10-12 17:06:23 +0200

    Fixed deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 FRONTEND_S3_BUCKET=$(doppler run --command 'echo $FRONTEND_S3_BUCKET')
 CLOUDFRONT_DISTRIBUTION_ID=$(doppler run --command 'echo $CLOUDFRONT_DISTRIBUTION_ID')
 
-doppler run yarn build &&
+NODE_OPTIONS=--openssl-legacy-provider doppler run yarn build &&
 aws s3 rm $FRONTEND_S3_BUCKET --recursive &&
 aws s3 cp build $FRONTEND_S3_BUCKET --recursive &&
 aws cloudfront create-invalidation --distribution-id $CLOUDFRONT_DISTRIBUTION_ID --paths  '/*'
